49e381e718431d90779bda6a5cd3c7be507814c5
BruhWaxie <efirestmax@gmail.com>
2025-09-04 19:00:20 +0300

    Fixed bugs

--- task_app/views.py
+++ task_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView
 from .models import Task, Comment
 from .forms import TaskForm
@@ -6,12 +7,14 @@ from .forms import TaskForm
 class TaskListView(ListView):
     model = Task
     template_name = 'task_list.html'
+    context_object_name = 'tasks'
 # Create your views here.
 
 class TaskCreateView(CreateView):
     model = Task
     template_name = 'task_created.html'
-    fields = ('title', 'description', 'status', 'priority', 'deadline', 'image', 'file')
+    success_url = reverse_lazy('Home')
+
 
     form_class = TaskForm
 
@@ -25,6 +28,7 @@ class TaskUpdateView(UpdateView):
     model = Task
     template_name = 'task_update.html'
     fields = ('title', 'description', 'status', 'priority', 'deadline', 'image', 'file')
+    success_url = reverse_lazy('Home')  
 
     form_class = TaskForm
 
